eafc3a9bc21e290906f8ae627bf3ff990ede1806
Harshavardhan <harshkale612@gmail.com>
2025-10-08 00:36:52 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,16 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.use("/home", (req, res) => {
-  res.send("You are on Home");
+app.use("/user", (req, res) => {
+  res.send("Pro User !!!");
 });
 
-app.use("/test", (req, res) => {
-  res.send("Tesing....");
-}),
-  app.use("/", (req, res) => {
-    res.send("Hello World");
-  });
 app.listen(7777, () => {
   console.log("Server listening to port 7777");
 });
