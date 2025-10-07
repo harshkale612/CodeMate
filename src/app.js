d28251da09a8791b638a947438c16c976c23042c
Harshavardhan <harshkale612@gmail.com>
2025-10-08 00:50:16 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
 
-app.use("/user", (req, res) => {
-  res.send("Pro User !!!");
+// app.use("/user", (req, res) => {
+//   res.send("Pro User !!!");
+// });
+
+app.get("/user", (req, res) => {
+  res.send("Get User");
+});
+
+app.post("/user", (req, res) => {
+  res.send("Post User");
+});
+
+app.patch("/user", (req, res) => {
+  res.send("Patch User");
+});
+app.delete("/user", (req, res) => {
+  res.send("Delete User");
 });
 
 app.listen(7777, () => {
