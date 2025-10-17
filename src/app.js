8a30eb4a418e7ea2c83e94923a99165d21b6240b
Harshavardhan <harshkale612@gmail.com>
2025-10-17 18:02:35 +0530

    Add middleware

--- src/app.js
+++ src/app.js
@@ -1,25 +1,23 @@
 const express = require("express");
 const app = express();
 
-// app.use("/user", (req, res) => {
-//   res.send("Pro User !!!");
-// });
-
-app.get("/user", (req, res) => {
-  res.send("Get User");
+app.use("/", (req, res, next) => {
+  // res.send("Hi");
+  next();
 });
-
-app.post("/user", (req, res) => {
-  res.send("Post User");
-});
-
-app.patch("/user", (req, res) => {
-  res.send("Patch User");
-});
-app.delete("/user", (req, res) => {
-  res.send("Delete User");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    // console.log("Res 1");
+    // res.send("First Respont");
+    next();
+  },
+  (req, res) => {
+    console.log("Res 2");
+    res.send("Second Respont");
+  }
+);
 
 app.listen(7777, () => {
-  console.log("Server listening to port 7777");
+  console.log("Server listening on port 7777");
 });
