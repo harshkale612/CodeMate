abe0898464f215e3effdd7f1bd3c5c8fce8eb0b7
Harshavardhan <harshkale612@gmail.com>
2026-05-14 18:22:03 +0530

    Create User

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const connectDB = require("../src/config/database");
+const User = require("./models/user");
 const app = express();
+
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Naval",
+    lastName: "Ravikant",
+    emailID: "naval@gmail.com",
+    age: 48,
+    password: "Pass@1234",
+  });
+  try {
+    await user.save();
+    res.send("New user created successfully");
+  } catch (err) {
+    res.status(400).send("Somthing went wrong " + err);
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Database connected successfully");
