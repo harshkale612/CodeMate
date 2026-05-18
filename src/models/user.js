9dc4b284c2d1e501d10e7f2edc8b00d83bf809ac
Harshavardhan <harshkale612@gmail.com>
2026-05-18 12:49:26 +0530

    Add validations using validator

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const validator = require("validator");
 const userSchema = new mongoose.Schema(
   {
     firstName: {
@@ -15,6 +15,11 @@ const userSchema = new mongoose.Schema(
       unique: true,
       lowercase: true,
       trim: true,
+      validate: (value) => {
+        if (!validator.isEmail(value)) {
+          throw new Error("Enter valid email address");
+        }
+      },
     },
     password: {
       type: String,
@@ -34,6 +39,11 @@ const userSchema = new mongoose.Schema(
     },
     photoUrl: {
       type: String,
+      validate:(value)=>{
+        if(!validator.isURL(value)){
+       throw new Error ("Photo URL is in valid")
+        }
+      }
     },
     about: {
       type: String,
